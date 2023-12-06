819e3c29c23d3364bb1874c0b2258ef2dd17aa46
vinaykummar707 <vinaykummar707@gmail.com>
2023-12-06 10:46:42 +0530

    s

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(router);
 
 app.listen(port, async () => {
+    
     const dbConnection = new DbConnection();
     await dbConnection.connectToDatabase();
     console.log(`Server running at http://localhost:${port}`);
