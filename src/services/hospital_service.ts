ea6199119b4c58c8003f7c70dd24bbbe9be9eb38
vinaykummar707 <vinaykummar707@gmail.com>
2023-12-08 12:38:07 +0530

    u

--- src/services/hospital_service.ts
+++
@@ -1,9 +0,0 @@
-import { HospitalRespository } from "../repositories/hospital_repository"
-
-export class HospitalService {
-    hospitalRepo = new HospitalRespository();
-
-    async getAllHospitals() {
-        return await this.hospitalRepo.getAllHospitals();
-    }
-}
