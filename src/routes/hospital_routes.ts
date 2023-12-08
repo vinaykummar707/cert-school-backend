15949e41360b11e9d308227c2849903bd369fe81
vinaykummar707 <vinaykummar707@gmail.com>
2023-12-08 12:40:11 +0530

    p

--- src/routes/hospital_routes.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import {HospitalController} from "../controllers/hospital_controller";
-
-export const hospital_router = Router();
-const hospital_controller = new HospitalController();
-
-hospital_router.get('/', hospital_controller.getAllHospitals);
-
