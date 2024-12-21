576f1b5cdb2d90b8c9975d4e3e31b21e2a53bf57
Vinay Kummar <vpanuganti@jetblue.com>
2024-12-21 20:18:16 +0530

    backend fix

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,4 @@
-import {Router} from "express";
+import { Router } from "express";
 import { studentRouter } from "./student_routes";
 import { activityRouter } from "./activity_routes";
 import { roleRouter } from "./role_routes";
@@ -8,44 +8,70 @@ import { readFile, writeFile } from "fs/promises";
 import { DbConnection } from "../connections/db_connection";
 export const router = Router();
 
+router.use("/student", studentRouter);
+router.use("/activity", activityRouter);
+router.use("/user", userRouter);
+router.use("/role", roleRouter);
+router.use("/certificate", certificateRouter);
+router.get("/config", async (req, res) => {
+  const data = await readFile("./config.json", "utf-8");
+  res.json(JSON.parse(data));
+});
 
+router.post("/config", async (req, res) => {
+  const data = await writeFile(
+    "./config.json",
+    JSON.stringify(req.body),
+    "utf-8"
+  );
+  res.json(data);
+});
 
-router.use('/student', studentRouter);
-router.use('/activity', activityRouter);
-router.use('/user', userRouter);
-router.use('/role', roleRouter);
-router.use('/certificate', certificateRouter);
-router.get('/config',async (req,res) => {
-    const data = await readFile("./config.json",'utf-8');
-    res.json(JSON.parse(data));
-})
+router.post("/top-students", async (req, res) => {
+  console.log(req.body, parseInt(req.body.year));
+  const sequelize = DbConnection.getSequelize;
+  const da: any = await sequelize.query(
+    "exec GetTopStudentInfo :EduPassed, :EduStream ,:yearPassed",
+    {
+      replacements: {
+        EduPassed: req.body.EducationPassed,
+        EduStream: null,
+        yearPassed: parseInt(req.body.year),
+      },
+      raw: true,
+    }
+  );
+  res.json(da);
+});
 
-router.post('/config',async (req,res) => {
-    const data = await writeFile('./config.json' ,JSON.stringify(req.body),'utf-8');
-    res.json(data);
-})
-
-router.get('/top-students/:edu/:year',async (req,res) => {
+router.get("/top-sports/:event/:year", async (req, res) => {
+    console.log(req.body, parseInt(req.body.year));
     const sequelize = DbConnection.getSequelize;
-    const da: any =await sequelize.query('exec GetTopStudentInfo :EduPassed, :yearPassed',
-        { replacements: { EduPassed: req.params.edu, yearPassed: parseInt(req.params.year)},
-          raw: true }
-      )
-      res.json(da);
-})
+    const da: any = await sequelize.query(
+      "exec GetTopSportsInfo :event, :eventYear",
+      {
+        replacements: {
+          event: req.params.event,
+          eventYear: parseInt(req.params.year),
+        },
+        raw: true,
+      }
+    );
+    res.json(da);
+});
+  
+router.get("/eduPassed", async (req, res) => {
+  const sequelize = DbConnection.getSequelize;
+  const da: any = await sequelize.query("exec GetEducationPassed", {
+    raw: true,
+  });
+  res.json(da);
+});
 
-router.get('/eduPassed',async (req,res) => {
-    const sequelize = DbConnection.getSequelize;
-    const da: any =await sequelize.query('exec GetEducationPassed',
-        {raw: true }
-      )
-      res.json(da);
-})
-
-router.get('/eduStream',async (req,res) => {
-    const sequelize = DbConnection.getSequelize;
-    const da: any =await sequelize.query('exec GetEducationStream',
-        {raw: true }
-      )
-      res.json(da);
-})
+router.get("/eduStream", async (req, res) => {
+  const sequelize = DbConnection.getSequelize;
+  const da: any = await sequelize.query("exec GetEducationStream", {
+    raw: true,
+  });
+  res.json(da);
+});
